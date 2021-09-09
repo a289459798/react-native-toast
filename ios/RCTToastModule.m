17f2de0497ab128602f1d374c3a1d47f97a650e1
zhangzy <zhangzy0816@gmail.com>
2021-09-09 14:52:10 +0800

    修改ios Toast

--- ios/RCTToastModule.m
+++ ios/RCTToastModule.m
@@ -23,27 +23,32 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(show:(NSString *)message)
 {
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        
+
         MBProgressHUD *HUD = [MBProgressHUD showHUDAddedTo:[UIApplication sharedApplication].keyWindow animated:YES];
         HUD.mode = MBProgressHUDModeText;
         HUD.label.textColor = [UIColor whiteColor];
         HUD.label.text = message;
         HUD.margin = 10.f;
         HUD.removeFromSuperViewOnHide = YES;
-        
+        HUD.userInteractionEnabled= NO;
+        HUD.animationType = MBProgressHUDAnimationZoom;
+        HUD.bezelView.color = [UIColor colorWithWhite:0.f alpha:0.6f];
+        // 设置模式
+        HUD.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
+
         [HUD hideAnimated:YES afterDelay:2];
-        
+
     });
-    
+
 }
 
 RCT_EXPORT_METHOD(showLoading:(NSString *)message)
 {
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        
+
         [self getHUD];
         HUD.mode = MBProgressHUDModeIndeterminate;
         HUD.activityIndicatorColor = [UIColor whiteColor];
@@ -51,20 +56,20 @@ RCT_EXPORT_METHOD(showLoading:(NSString *)message)
         HUD.label.text = message;
         HUD.removeFromSuperViewOnHide = YES;
         [HUD showAnimated:YES];
-        
+
     });
-    
+
 }
 
 RCT_EXPORT_METHOD(hide)
 {
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        
+
         [HUD hideAnimated: YES];
         HUD = nil;
     });
-    
+
 }
 
 @end
